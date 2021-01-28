65ce14e8a3524620f599d21ba6676b1fd630496f
mervebingool <mervebingool@gmail.com>
2021-01-28 21:23:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace YazilimKampı3gOdev2
+{
+   
+    class Referans
+    {
+       
+        public void Kare(ref int i, ref int k)
+        {
+            i = i * k;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Referans rf = new Referans();
+
+            int a = 10;
+            int b = 20;
+            Console.WriteLine("---------REF KULLANIMI-----------------");
+
+            Console.WriteLine("Çağrıdan önce: " + a);
+
+            
+            rf.Kare(ref a, ref b);
+
+            Console.WriteLine("Çağrıdan sonra: " + a);
+
+            Console.WriteLine("-----------------------------------------");
+        }
+    }
+}
